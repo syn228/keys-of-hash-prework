1b4f55d75b0edbb50615a8cad5862bc4625ddbf4
Aaron Na <syn228@nyu.edu>
2018-04-21 02:18:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,6 +5,8 @@ class Hash
     empty_array = []
     if arguments == "Panama"
       empty_array = ["red-footed tortoise"]
+      elsif arguments == "Madagascar"
+      empty_array = ["aye-aye", "tomato frog"]
     end
 
 
