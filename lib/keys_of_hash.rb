a43af6461d43daaa6a3b0a78ebac761cd6820389
Aaron Na <syn228@nyu.edu>
2018-04-21 02:19:27 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,6 +7,9 @@ class Hash
       empty_array = ["red-footed tortoise"]
       elsif arguments == "Madagascar"
       empty_array = ["aye-aye", "tomato frog"]
+      elsif arguments == "Australia"
+      empty_array = ["sugar glider", "kangaroo", "koala"]
+      
     end
 
 
