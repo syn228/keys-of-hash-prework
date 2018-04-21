360735b199792d1ab148b2404facc6052c3ffd35
Aaron Na <syn228@nyu.edu>
2018-04-21 02:23:02 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 class Hash
   def keys_of(arguments)
-    empty_array = []
-    if arguments == "Panama"
-      empty_array = ["red-footed tortoise"]
-      elsif arguments == "Madagascar"
-      empty_array = ["aye-aye", "tomato frog"]
-      elsif arguments == "Australia"
-      empty_array = ["sugar glider", "kangaroo", "koala"]
-      
+  array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
     end
-
-
+    return array
   end
 end
